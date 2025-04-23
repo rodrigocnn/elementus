ef65cbdf39ce081adf263aa6d76f4d1cfa1fd0e8
Rodrigo Cesar <rodrigocnn@gmail.com>
2025-04-23 17:05:35 -0300

    refactor: refactor package react

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { colors, spacing, fonts } from "@elementus/tokens";
 
-interface ButtonProps {
+export interface ButtonProps {
   label: string;
   onClick: () => void;
 }
